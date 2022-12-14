f793592bac5b01321ba0ee17cf7d53f6abd679b2
crusher8010 <crusher8010@gmail.com>
2022-12-14 22:47:07 +0530

    resolved conflicts

--- zee/src/App.js
+++ zee/src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import AllRoutes from './routs/AllRoutes';
 import Navbar from "./components/rittik_navbar/Navbar";
-import AllRoutes from "./routs/AllRoutes";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Navbar />
+      <AllRoutes />
     </div>
   );
 }
