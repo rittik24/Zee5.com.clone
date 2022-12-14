f6e0fae1d1cc26644c155721d4651af7807a477e
Rittik Haldar <rickhaldar30@gmail.com>
2022-12-14 20:46:29 +0530

    react-icon installed

--- zee/src/App.js
+++ zee/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Home from './components/anirudh_home/Home';
+import Navbar from './components/rittik_navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Home />
     </div>
   );
