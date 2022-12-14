a3a98307d8f6cc37b46d13a6d96ba88029325961
Moumita Das <dasmoumita.me@gmail.com>
2022-12-14 21:38:37 +0530

    buypage structure

--- zee/src/App.js
+++ zee/src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+// import './App.css';
 import Home from './components/anirudh_home/Home';
+import Buyplan from './components/moumita_buyplan/Buyplan';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <Buyplan/>
     </div>
   );
 }
