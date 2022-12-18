9b50d03f001b496c04f94c57fdeac07d354c096a
Rittik Haldar <rickhaldar30@gmail.com>
2022-12-19 00:54:35 +0530

    changes

--- zee/src/components/rittik_navbar/Navbar.jsx
+++ zee/src/components/rittik_navbar/Navbar.jsx
@@ -7,8 +7,8 @@ import {
 import "./navbar.css";
 import { Link } from 'react-router-dom'
 import { GiHamburgerMenu } from "react-icons/gi";
-
-const admin= [
+import zeelogo from "../../images/ZEEAPP.png"
+const admin = [
   {
     "id": 1,
     "type": "admin",
@@ -56,10 +56,10 @@ function Navbar() {
   const [barsIcon, setBarsIcon] = useState("block");
   const [navs, setNavs] = useState("none");
   const { isOpen, onOpen, onClose } = useDisclosure()
-  const {state, handlelogout} = useContext(authContext);
+  const { state, handlelogout } = useContext(authContext);
 
   // console.log(change, token)
-  
+
   function bars() {
     setBarsIcon("none");
     setNavs("flex");
@@ -75,33 +75,35 @@ function Navbar() {
     <div className="navv">
 
       <div className='imgg'>
-        <Link to='./'> <img src="https://scontent.fccu31-1.fna.fbcdn.net/v/t39.30808-6/319993825_849692672750825_2848840103300883038_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=Y5_7UGjMX9YAX_M-29r&_nc_ht=scontent.fccu31-1.fna&oh=00_AfC_kaxOBf8ldOkfYRVxDKnM7kirUAFSgeGXhTb-noANvw&oe=639DFE06" alt="ZeeApp Logo" className="nav-logo" /></Link>
+        <Link to='./'> <img src={zeelogo} alt="ZeeApp Logo" className="nav-logo" /></Link>
       </div>
-      <div className='navv-linkss' id="navv-links">
+      <div className='navv-links' id="navv-links">
         <Link to='/'><h4>Home</h4></Link>
         <Link to='/tvshows'><h4>TV Shows</h4></Link>
         <Link to='/moviespage'><h4>Movies</h4></Link>
         <Link to='/webseriespage'><h4>Web Series</h4></Link>
         <Link to='/newspage'><h4>News</h4></Link>
       </div>
+
+      <div className='input-div'>
+        {/* <i className="fas fa-search"></i> */}
+        <input type="text" placeholder='Search for Movies ,Shows etc..' />
+      </div>
+
       <div className='navv-linkss' id="navv-link">
-        <div className='input-div'>
-          <i className="fas fa-search"></i>
 
-          <input type="text" placeholder='Search for Movies ,Shows etc..' />
-
-        </div>
-        <i className="fas fa-download"></i>
+        {/* <i className="fas fa-download"></i> */}
 
         <Box className="NavbarSecondPartV-Lan" ><Flex><Text>A</Text><Text className="NavbarSecondPartV-LanHindi">अ</Text></Flex> </Box>
-        {state.isAuth == false ? null: state.token.includes("zee") ? <Link to='/admindashboard'> <button className='admin-button'>ADMIN</button></Link> :
-        <Link to='/userdashboard'> <button className='admin-button'>USER</button></Link> } 
-      
-        
-          {state.isAuth == true ? <Link to='/login'><button className='login-button' onClick={handlelogout}>LOGOUT</button></Link> : <Link to='/login'><button className='login-button'>LOGIN </button></Link> }
-        
+        {state.isAuth == false ? null : state.token.includes("zee") ? <Link to='/admindashboard'> <button className='admin-button'>ADMIN</button></Link> :
+          <Link to='/userdashboard'> <button className='admin-button'>USER</button></Link>}
+
+
+        {state.isAuth == true ? <Link to='/login'><button className='login-button' onClick={handlelogout}>LOGOUT</button></Link> : <Link to='/login'><button className='login-button'>LOGIN </button></Link>}
+
         <Link to='/subscription'><button className='buy-button'>BUY PLAN</button></Link>
       </div>
+      
       <div className='hum'>
         <Flex alignItems="center" className="hum-Menu" >
           <Button colorScheme='blue' onClick={onOpen}>
